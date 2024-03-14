eb125db3143e361f4cc89871d80ccfae07986501
Hamidreza <manafi.hamidreza@gmail.com>
2024-03-14 11:11:39 +0330

    Add Domain classes

---
+++ app/src/main/java/com/example/movie/domain/Genres.java
@@ -0,0 +1,19 @@
+
+package com.example.movie.domain;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class Genres {
+    private List<GenresItem> genres;
+
+    public List<GenresItem> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(List<GenresItem> genres) {
+        this.genres = genres;
+    }
+}
